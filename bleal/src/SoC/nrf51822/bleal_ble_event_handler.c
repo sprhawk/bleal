ff4d5ddcae55190222138f63d2453e65c960ca51
sprhawk <hongbo@yang.me>
2014-09-21 11:45:13 +0800

    added ble event handler

---
+++ bleal/src/SoC/nrf51822/bleal_ble_event_handler.c
@@ -0,0 +1,35 @@
+/* File: bleal_ble_event_handler.c
+ * Author: YangHongbo<hongbo@yang.me>
+ * Created at: Sep 19th, 2014
+ *
+ * Copyright (c) 2014, Yang Hongbo (hongbo@yang.me) 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "bleal_ble_event_handler.h"
+
+#include "bleal_nrf51822.h"
+
+void ble_on_event_handler(ble_evt_t * p_ble_evt)
+{
+    switch(p_ble_evt->header.evt_id) {
+        default:
+            break;
+    }
+}
